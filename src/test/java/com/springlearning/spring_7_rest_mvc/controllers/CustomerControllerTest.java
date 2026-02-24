0376f15a79416cc5185ea4b3254acc4983bb66da
Giridaran K K <121053470+GiridaranKK@users.noreply.github.com>
2026-02-24 23:13:40 +0400

    ASSIGNMENT 13 - TEST DELETE CUSTOMER

--- src/test/java/com/springlearning/spring_7_rest_mvc/controllers/CustomerControllerTest.java
+++ src/test/java/com/springlearning/spring_7_rest_mvc/controllers/CustomerControllerTest.java
@@ -2,6 +2,7 @@ package com.springlearning.spring_7_rest_mvc.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
@@ -19,12 +20,13 @@ import com.springlearning.spring_7_rest_mvc.model.Customer;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -102,5 +104,19 @@ public class CustomerControllerTest {
 				.andExpect(status().isNoContent());
 		
 		verify(customerService).updateCustomer(any(UUID.class), any(Customer.class));
+	}
+	
+	@Test
+	void deleteCustomerById() throws Exception {
+		Customer customer = customerServiceImpl.listCustomers().get(0);
+		
+		mockMvc.perform(delete("/api/v1/customer/" + customer.getId())
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNoContent());
+		
+		ArgumentCaptor<UUID> uuidArgumentCaptor = ArgumentCaptor.forClass(UUID.class);
+		verify(customerService).deleteCustomerById(uuidArgumentCaptor.capture());
+		assertThat(customer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
+		
 	}
 }
