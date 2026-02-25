ee4d1dfec4988a7f8ef87220697f2d626448bfcf
Giridaran K K <121053470+GiridaranKK@users.noreply.github.com>
2026-02-25 17:42:55 +0400

    Exception handling in spring boot (Ir is not a recommended way)

--- src/test/java/com/springlearning/spring_7_rest_mvc/controllers/BeerControllerTest.java
+++ src/test/java/com/springlearning/spring_7_rest_mvc/controllers/BeerControllerTest.java
@@ -159,5 +159,12 @@ public class BeerControllerTest {
 		assertThat(beerMap.get("beerName")).isEqualTo(beerArgumentCaptor.getValue().getBeerName());
 	}
 	
+	@Test
+	void getBeerByIdNotFound() throws Exception {
+		
+		given(beerService.getBeerById(any(UUID.class))).willThrow(NotFoundException.class);
+		mockMvc.perform(get(BeerController.BEER_PATH_ID,UUID.randomUUID()))
+		.andExpect(status().isNotFound());
+	}
 	
 }
