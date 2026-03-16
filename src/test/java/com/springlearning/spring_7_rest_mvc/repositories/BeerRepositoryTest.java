89c84d84d974374fc8bcf04a78a6d110d52f209b
Giridaran K K <121053470+GiridaranKK@users.noreply.github.com>
2026-03-16 18:28:00 +0400

    constraint validation exception

--- src/test/java/com/springlearning/spring_7_rest_mvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/springlearning/spring_7_rest_mvc/repositories/BeerRepositoryTest.java
@@ -1,8 +1,9 @@
 package com.springlearning.spring_7_rest_mvc.repositories;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
+
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 import com.springlearning.spring_7_rest_mvc.entities.Beer;
 import com.springlearning.spring_7_rest_mvc.model.BeerStyle;
+
+import jakarta.validation.ConstraintViolationException;
 
 @DataJpaTest
 public class BeerRepositoryTest {
@@ -19,16 +22,18 @@ public class BeerRepositoryTest {
 	
 	@Test
 	void testSaveBeer() {
-		Beer savedBeer = beerRepository.save(Beer.builder()
-				.beerName("beerName1")
-				.beerStyle(BeerStyle.BEERSTRYLE1)
-				.upc("1234566")
-				.price(new BigDecimal(25.00))
-				.build());
 		
-		beerRepository.flush();
+		assertThrows(ConstraintViolationException.class, () ->{
+			Beer savedBeer = beerRepository.save(Beer.builder()
+					.beerName("beerName1 jksaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+					.beerStyle(BeerStyle.BEERSTRYLE1)
+					.upc("1234566")
+					.price(new BigDecimal(25.00))
+					.build());
+			beerRepository.flush();
+		});
 		
-		assertThat(savedBeer).isNotNull();
-		assertThat(savedBeer.getId()).isNotNull();
+//		assertThat(savedBeer).isNotNull();
+//		assertThat(savedBeer.getId()).isNotNull();
 	}
 }
