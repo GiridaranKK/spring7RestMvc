5ebc94f40639433a85902eb7ac4867cbe74be22d
Giridaran K K <121053470+GiridaranKK@users.noreply.github.com>
2026-03-20 01:30:23 +0400

    added service connection annotation

--- src/test/java/com/springlearning/spring_7_rest_mvc/repositories/MySqlTest.java
+++ src/test/java/com/springlearning/spring_7_rest_mvc/repositories/MySqlTest.java
@@ -9,6 +9,7 @@ import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -24,17 +25,8 @@ import com.springlearning.spring_7_rest_mvc.entities.Beer;
 public class MySqlTest {
 	
 	@Container
+	@ServiceConnection
 	static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0.36");
-	
-	@DynamicPropertySource
-	static void mySqlProperties(DynamicPropertyRegistry registry) {
-	    registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
-	    registry.add("spring.datasource.username", mySQLContainer::getUsername);
-	    registry.add("spring.datasource.password", mySQLContainer::getPassword);
-	}
-	
-	@Autowired
-	DataSource dataSource;
 	
 	@Autowired
 	BeerRepository beerRepository;
