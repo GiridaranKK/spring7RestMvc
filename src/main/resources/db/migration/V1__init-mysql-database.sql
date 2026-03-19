c94b904c930210834b44883ca3a51dae7b3cd7d2
Giridaran K K <121053470+GiridaranKK@users.noreply.github.com>
2026-03-19 23:06:10 +0400

    changes done for flyway to create table and history

---
+++ src/main/resources/db/migration/V1__init-mysql-database.sql
@@ -0,0 +1,26 @@
+
+    drop table if exists beer;
+
+    drop table if exists customer;
+
+    create table beer (
+        beer_style smallint not null check ((beer_style between 0 and 3)),
+        price decimal(38,2) not null,
+        version integer,
+        created_date datetime(6),
+        updated_date datetime(6),
+        id varchar(36) not null,
+        beer_name varchar(50) not null,
+        quantity_on_hand varchar(255),
+        upc varchar(255) not null,
+        primary key (id)
+    ) engine=InnoDB;
+
+    create table customer (
+        version integer,
+        created_date datetime(6),
+        last_modifieddate datetime(6),
+        id varchar(36) not null,
+        customer_name varchar(60) not null,
+        primary key (id)
+    ) engine=InnoDB;
