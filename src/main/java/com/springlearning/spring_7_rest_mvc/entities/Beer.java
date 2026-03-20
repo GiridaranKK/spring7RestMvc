5c56fc500908141d35a4cbe2dbe3d4a0c208fb88
Giridaran K K <121053470+GiridaranKK@users.noreply.github.com>
2026-03-20 19:52:46 +0400

    create date and update date annotation to autopopulate

--- src/main/java/com/springlearning/spring_7_rest_mvc/entities/Beer.java
+++ src/main/java/com/springlearning/spring_7_rest_mvc/entities/Beer.java
@@ -5,8 +5,10 @@ import java.sql.SQLType;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.type.SqlTypes;
 
@@ -62,6 +64,9 @@ public class Beer {
 	private Integer quantityOnHand;
 	@NotNull
 	private BigDecimal price;
+	
+	@CreationTimestamp
 	private LocalDateTime createdDate;
+	@UpdateTimestamp
 	private LocalDateTime updatedDate;
 }
